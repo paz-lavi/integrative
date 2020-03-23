03efd68e7ed4af06da7ba545305e6b56c0b3a5e9
Or Shulrufer <pumaor23@gmail.com>
2020-03-23 11:12:01 +0100

    or

--- src/main/java/demo/UserImplementation.java
+++ src/main/java/demo/UserImplementation.java
@@ -8,29 +8,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserImplementation implements UserInterface{
-	
-    private List<User> users = new ArrayList<User>(); 
+
 	private String domain = "2020.demo";
 
 	@Override
 	public UserBoundry createNewUser(String email, String role, String username, String avatar) {
 		UserId userId = new UserId(domain, email);
-		User user = new User(userId, role, username, avatar);
-		users.add(user);
 		return new UserBoundry(userId, role, username, avatar);
 	}
 
 	@Override
 	public UserBoundry loginAndRetrieveUserDitails(String userDomain, String userEmail){
-		UserId userId = new UserId(userDomain, userEmail);
-		User user = this.users.stream().filter(u -> u.getUserId().equals(userId)).collect(Collectors.toList()).get(0);
-		return new UserBoundry(user.getUserId(), user.getRole(), user.getUsername(), user.getAvatar());
+		return null;
 	}
 
 	@Override
 	public UserBoundry updateUserDitails(String userDomain, String userEmail, String email, String role,
 			String username, String avatar) {
-		UserId userId = new UserId(userDomain, userEmail);
 		return null;
 	}
 	
