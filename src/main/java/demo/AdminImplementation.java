0c29bfb8182cf90dd77dfa6a90bd44b2eed18225
paz-lavi <pazlavi29@gmail.com>
2020-03-31 16:37:43 +0300

    paz

---
+++ src/main/java/demo/AdminImplementation.java
@@ -0,0 +1,33 @@
+package demo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminImplementation implements AdminInterface {
+
+
+    @Override
+    public void deleteAllUsersInTheSystem(String adminDomain, String adminEmail) {
+
+    }
+
+    @Override
+    public void deleteAllElementsInTheSystem(String adminDomain, String adminEmail) {
+
+    }
+
+    @Override
+    public void deleteAllActionsInTheSystem(String adminDomain, String adminEmail) {
+
+    }
+
+    @Override
+    public UserBoundry exportAllUsers(String adminDomain, String adminEmail) {
+        return null;
+    }
+
+    @Override
+    public ActionBoundary exportAllActions(String adminDomain, String adminEmail) {
+        return null;
+    }
+}
