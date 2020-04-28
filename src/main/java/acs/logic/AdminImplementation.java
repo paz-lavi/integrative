f1bb833894866824275655b0f10e34b4be318f55
97254 <97254@DESKTOP-J9VDLF7>
2020-04-28 16:24:48 +0300

    michael

--- src/main/java/acs/logic/AdminImplementation.java
+++ src/main/java/acs/logic/AdminImplementation.java
@@ -1,36 +1,44 @@
 package acs.logic;
 
+
 import acs.rest.boudanries.ActionBoundary;
 import acs.rest.boudanries.UserBoundary;
+
+
+
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class AdminImplementation implements AdminService {
-
+	private ActionImplementation actionImplementaion;
+	private UserImplementation userImplementaion;
+	private ElementImplementation elementImplementaion;
+	
+	
 
     @Override
     public void deleteAllUsersInTheSystem(String adminDomain, String adminEmail) {
-
-    }
+    		userImplementaion.deleteAllUsers(adminDomain, adminEmail);
+    	}
 
     @Override
     public void deleteAllElementsInTheSystem(String adminDomain, String adminEmail) {
-
+    	elementImplementaion.deleteAllElements(adminDomain, adminEmail);
     }
 
     @Override
     public void deleteAllActionsInTheSystem(String adminDomain, String adminEmail) {
-
+    	actionImplementaion.deleteAllActions(adminDomain, adminEmail);
     }
 
     @Override
     public UserBoundary exportAllUsers(String adminDomain, String adminEmail) {
-        return null;
+    	return (UserBoundary) userImplementaion.getAllUsers(adminDomain, adminEmail);
     }
 
     @Override
     public ActionBoundary exportAllActions(String adminDomain, String adminEmail) {
-        return null;
+        return (ActionBoundary) actionImplementaion.getAllActions(adminDomain, adminEmail);
     }
 }
