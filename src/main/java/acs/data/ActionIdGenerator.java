8ce3cbdff8675f2d8ed250da247bb292c91ff1c7
Yahel <Yahel@DESKTOP-V75MPNR>
2020-05-06 16:19:56 +0300

    action generator

---
+++ src/main/java/acs/data/ActionIdGenerator.java
@@ -0,0 +1,14 @@
+package acs.data;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.stereotype.Component;
+@Component
+public class ActionIdGenerator {
+	  private static final AtomicInteger counter = new AtomicInteger();
+
+	  public static int nextValue() {
+	    return counter.getAndIncrement();
+	  }
+
+}
