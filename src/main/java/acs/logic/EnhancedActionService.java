f3f911ea7d480257c6adbe0e813ab260b9e3b9f6
Yahel <Yahel@DESKTOP-V75MPNR>
2020-05-18 20:51:43 +0300

    user and action enhanced

---
+++ src/main/java/acs/logic/EnhancedActionService.java
@@ -0,0 +1,11 @@
+package acs.logic;
+
+import java.util.List;
+
+import acs.rest.boudanries.ActionBoundary;
+
+public interface EnhancedActionService extends ActionService{
+	
+    public List<ActionBoundary> getAllActions(String adminDomain, String adminEmail,int size,int page);
+
+}
